62f7a3fe10dc48318d0406e01fae6b88dcabc58b
kimo741 <kimoelgamal741@gmail.com>
2022-04-30 16:04:52 +0200

    first commit

---
+++ src/pages/pageCamera.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <h2>Camira page</h2>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageCamira",
+});
+</script>
